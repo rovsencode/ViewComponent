454568f2a77555acbee4175cebac4742b0fb9ebb
rovsencode <rovshanakh@code.edu.az>
2023-02-27 16:34:29 +0400

    Add Product View Component

---
+++ FirelloProject/ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using FirelloProject.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirelloProject.ViewComponents
+{
+
+    public class ProductViewComponent:ViewComponent
+    {
+               private readonly AppDbContext _appDbContext;
+
+            public ProductViewComponent(AppDbContext appDbContext)
+            {
+                _appDbContext = appDbContext;
+            }
+
+            public async Task<IViewComponentResult> InvokeAsync(int take)
+            {
+              var products = _appDbContext.Products
+                .Include(p=>p.Category).
+                Include(p=>p.ProductImages).Take(take).ToList();
+                return View(await Task.FromResult(products));
+            }
+        }
+    
+}
